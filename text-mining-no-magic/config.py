ebb1bf92d1baa5a3baad1696a3f27b6ce5dd2a06
Samuel M.H <samuel.munoz@beeva.com>
2017-01-12 10:08:36 +0100

    Técnicas de preprocesado

---
+++ text-mining-no-magic/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+#
+# # Edit this
+#
+
+BASE_PATH = '/home/samuelmunoz/beeva/workshop-nlp'
+
+
+#
+# # Datasets
+#
+
+DATASET_MUCHOCINE_RAW = BASE_PATH+'/datasets/muchocine'
+DATASET_MUCHOCINE = BASE_PATH+'/datasets/muchocine.json.zip'
